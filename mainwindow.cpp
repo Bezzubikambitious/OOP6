704dbf9fab9d5f43a8c62fabe6a87b7f983cbbd4
Bezzubikambitious <61292027+Bezzubikambitious@users.noreply.github.com>
2020-03-20 21:31:50 +0200

    Tania

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -15,13 +15,6 @@ MainWindow::~MainWindow()
 }
 
 
-void MainWindow::on_pushButton_clicked()
-{
-
-}
-
-
-
 
 void MainWindow::on_set_clicked()
 {
@@ -33,4 +26,41 @@ void MainWindow::on_set2_clicked()
 {
     this->m2.SetDolar(ui->set_dollars2->text().toInt());
     this->m2.SetCent(ui->set_cents2->text().toInt());
+}
+
+void MainWindow::on_sum_clicked()
+{
+    this->res = this->m1.operator+(this->m2);
+    ui->show_sum->setText(QString::fromStdString(this->res.get_res()));
+}
+
+void MainWindow::on_sub_clicked()
+{
+    this->res = this->m1.operator-(this->m2);
+    ui->show_sub->setText(QString::fromStdString(this->res.get_res()));
+}
+
+
+void MainWindow::on_multi_clicked()
+{
+    this->res = this->m1.operator*(this->m2);
+    ui->show_mul->setText(QString::fromStdString(this->res.get_res()));
+}
+
+void MainWindow::on_div_clicked()
+{
+    this->res = this->m1.operator*(this->m2);
+    ui->show_div->setText(QString::fromStdString(this->res.get_res()));
+}
+
+void MainWindow::on_right_clicked()
+{
+    this->res = this->m1.operator>>(this->m2);
+    ui->show_right->setText(QString::fromStdString(this->res.get_res()));
+}
+
+void MainWindow::on_left_clicked()
+{
+    this->res = this->m1.operator<<(this->m2);
+    ui->show_left->setText(QString::fromStdString(this->res.get_res()));
 }
